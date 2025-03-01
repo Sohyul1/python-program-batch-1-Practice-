9e0b6d237f5b59a9095a0f0403bf2ad75577c121
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 19:45:59 +0800

    done

--- Prog_5.py
+++ Prog_5.py
@@ -1,5 +1,16 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 # Ask for user input
+while True:
+    num1 = float(input("Enter your dividend: "))
+    num2  = float(input("Enter the divisor: "))
+
 # Get the quotient
+    quotient = num1 / num2
 # Print the quotient
-# Loop
+    print(f"The quotient of the given numbers is {quotient}.")
+
+    restart = input("\nWanna try again?(yes/no):").lower()
+    # Loop
+    if restart == "no":
+        print("Good bye!")
+        break
