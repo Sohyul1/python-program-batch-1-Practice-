64927d9cc63ab8b85f5376c07249c7d421094354
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 19:00:22 +0800

    looping, done

--- Prog_1.py
+++ Prog_1.py
@@ -1,15 +1,25 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-# Ask the user to input a number
-num1 = float(input("Enter a number: "))
-num2  = float(input("Enter another number: "))
 
-# Determine the biiger number
-# Print the bigger number if there is 
-if num1 > num2:
-    print(f"{num1} is the biger number")
-elif num2 > num1:
-    print(f"{num2} is the bigger number")
-else:
-    print("The numbers you entered are equals")
+# Ask the user to input a number
+while True:
+    num1 = float(input("Enter a number: "))
+    num2  = float(input("Enter another number: "))
+
+    # Determine the bigger number
+    # Print the bigger number if there is else ask for another input
+    if num1 > num2:
+        print(f"{num1} is the bigger number.")
+    elif num2 > num1:
+        print(f"{num2} is the bigger number.")
+    else:
+        print("The numbers you entered are equal.")
+
+    ans = input("\n(Type restart/exit): ").lower().strip()
+    if ans != "restart":
+        print("Good bye")
+        break
+        
+       
+ 
 
 
