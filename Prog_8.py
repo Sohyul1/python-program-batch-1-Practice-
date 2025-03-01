4e892af0403a0056f167da1dae0240517d1218e9
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 20:31:02 +0800

    done

--- Prog_8.py
+++ Prog_8.py
@@ -1,6 +1,28 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 # Make an empty list
 # Ask for user input
-# Filter out the odd numbers  
-# Print the odd numbers
-# Loop 
+numbers = []
+while True:
+    for nums in range(1, 11):
+        num = float(input(f"Enter number {nums}: "))
+        # Filter out the odd numbers
+        if num % 2 != 0:
+            numbers.append(num)
+    
+    total = len(numbers)
+    # Print the odd numbers
+    print(f"There is a total of {total} odd numbers.")
+    
+    # Loop
+    ans = input("Wanna go again?(yes/no): ")
+    if ans == "no":
+        print("Good Bye!")
+        break
+
+    
+
+
+
+
+  
+
