3968f4a5dcaf096910977c299f13711f8d0aac4c
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 18:27:41 +0800

    algo

---
+++ Prog_1.py
@@ -0,0 +1,4 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+# Ask the user to input a number
+# Determine the biiger number
+# Print the bigger number if there is 
