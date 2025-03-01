949c483d4226312db7bab748c7322a76dca9f9ac
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 19:10:51 +0800

    algo

---
+++ Prog_2.py
@@ -0,0 +1,4 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+# Ask the user to input a number
+# Determine the numbers are equal
+# Print the output
