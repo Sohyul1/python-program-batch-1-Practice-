ce44dd1660dfebbdc1cc8aa746fc8fce89615f48
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 19:37:23 +0800

    done

--- Prog_4.py
+++ Prog_4.py
@@ -1,5 +1,16 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 # Ask for user input
+while True:
+    num1 = float(input("Enter a number: "))
+    num2  = float(input("Enter another number: "))
+
 # Get the product
-# Print the prodcut
-# Loop
+    product = num1 * num2
+# Print the product
+    print(f"The product of the given numbers is {product}")
+
+    restart = input("\nWanna try again?(yes/no):").lower()
+    # Loop
+    if restart == "no":
+        print("Good bye!")
+        break
