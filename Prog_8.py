d076b7bd4b50b813b1844bbe249ae187fc1285c6
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 20:23:12 +0800

    algo

---
+++ Prog_8.py
@@ -0,0 +1,6 @@
+# Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+# Make an empty list
+# Ask for user input
+# Filter out the odd numbers  
+# Print the odd numbers
+# Loop 
