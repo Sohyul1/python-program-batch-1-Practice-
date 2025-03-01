dcdae6501d4a200aa43eca06fb3c7f2544e996fa
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 20:46:14 +0800

    algo

---
+++ Prog_10.py
@@ -0,0 +1,4 @@
+# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+# Make an empty list
+# Filter out the numbers ending with 0 
+# Print the rest of the  numbers
