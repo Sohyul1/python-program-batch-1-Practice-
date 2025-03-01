940e7e7dcb36346f1bfdeb9f51d1e2c604e98243
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 19:17:44 +0800

    done

--- Prog_2.py
+++ Prog_2.py
@@ -1,4 +1,19 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
 # Ask the user to input a number
+while True:
+    num1 = float(input("Enter a number: "))
+    num2  = float(input("Enter another number: "))
+
 # Determine the numbers are equal
 # Print the output
+    if num1 == num2:
+        print("Equal")
+    else:
+        print("Not equal")
+
+    ans = input("Try again?(yes/no): ").lower()
+    if ans == "no":
+        print("Good bye!")
+        break
+
