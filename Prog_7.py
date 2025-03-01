120d8d1d670b88fe5e3fb554edddad40cce0a376
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 20:19:44 +0800

    done

--- Prog_7.py
+++ Prog_7.py
@@ -1,6 +1,17 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 # Make an empty list
+numbers = []
 # Ask for user input
+while True:
+    for nums in range(1, 11):
+        num = float(input(f"Enter number {nums}: "))
+        numbers.append(num)
 # Get the sum 
-# Print The sum
+    total = sum(numbers)
+    # Print The sum
+    print(f"The sum of the given numbers is {total}.")
 # Loop 
+    ans = input("Wanna add again?(yes/no): ")
+    if ans == "no":
+        print("Good Bye!")
+        break
