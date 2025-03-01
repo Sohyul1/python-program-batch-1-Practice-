a90cfc0a3e22314d69680055568b4ff648a0c417
Sohyul1 <aerondequito967@gmail.com>
2025-03-01 20:04:20 +0800

    done

--- Prog_6.py
+++ Prog_6.py
@@ -1,5 +1,16 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 # Ask for user input
-# Get the sqaured of the numbers 
-# Print the output 
-# Loop
+while True:
+    num1 = float(input("Enter the base number: "))
+    num2  = float(input("Enter the exponent: "))
+    
+# Get the sqaured of the numbers
+    expo = num1 ** num2
+    # Print the output 
+    print(f"The squared value of the given numbers is {expo}.")
+
+    restart = input("\nWanna try again?(yes/no):").lower()
+    # Loop
+    if restart == "no":
+        print("Good bye!")
+        break
